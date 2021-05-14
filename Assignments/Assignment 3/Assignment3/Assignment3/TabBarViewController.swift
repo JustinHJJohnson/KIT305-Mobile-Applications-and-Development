33702a27d8b74b416cfef21aed816992ee81b6f0
Justin Johnson <justin.h.j.johnson@gmail.com>
2021-05-14 14:36:43 +1000

    Fixed TabView not being full screen
    Had to change the presentation to fullscreen

--- Assignments/Assignment 3/Assignment3/Assignment3/TabBarViewController.swift
+++ Assignments/Assignment 3/Assignment3/Assignment3/TabBarViewController.swift
@@ -10,7 +10,7 @@ import Firebase
 import FirebaseFirestoreSwift
 
 var students = [Student]()
-var weeks:[String] = ["Week 1", "Week 2", "Week 1", "Week 2", "Week 1", "Week 2", "Week 1", "Week 2", "Week 1", "Week 2", "Week 1", "Week 2"]
+var weeks:[String] = ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8", "Week 9", "Week 10", "Week 11", "Week 12"]
 
 class TabBarViewController: UITabBarController
 {
